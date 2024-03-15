9fb99bf4972799aa9f53545331bfa4c3d49563f5
Kip Cole <kipcole9@gmail.com>
2024-03-16 08:38:05 +1100

    Update test data

--- test/person_name_format_test.exs
+++ test/person_name_format_test.exs
@@ -41,14 +41,20 @@ defmodule Cldr.PersonName.FormatTest do
   |> Enum.uniq()
 
   # These are work in progress
-  # @test_locales []
+  @test_locales [
+    :gu, :hi, :hi_Latn, :km, :kn, :kok, :ml, :mr, :my, :ne, :or,
+    :pa, :si, :ta, :te, :as,
+
+    # Data error?
+    :es_US
+  ]
 
   for test <- Cldr.PersonName.TestData.parse_locales(@test_locales),
       test.line in @tests do
     with {:ok, test_locale} <- AllBackend.Cldr.validate_locale(test.locale),
          {:ok, name_locale} <- AllBackend.Cldr.validate_locale(test.name.locale) do
       name = Map.put(test.name, :locale, name_locale)
-      params = test.params ++ [locale: test_locale]
+      params = [{:locale, test_locale} | test.params]
       test_name = "#{test.locale}@#{test.line} with params #{inspect(test.params)}"
 
       test test_name do
